5b9aa11cabc7f801d74be6a220d62b8103c4c5c6
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-03 16:14:09 +0300

    magic strings moved

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Car added successfully";
+        public static string CarNameInvalid = "The car name must be at least two characters.";
+        public static string CarDailyPriceInvalid = "The daily price of the car must be at least zero.";
+        public static string CarDeleted = "Car deleted successfully";
+        public static string CarUpdated = "Car updated successfully";
+        public static string CarsListed = "Cars listed successfully";
+        public static string CarDetailsListed = "Car details listed successfully";
+
+        public static string BrandAdded = "Brand added successfully";
+        public static string BrandDeleted = "Brand deleted successfully";
+        public static string BrandUpdated = "Brand updated successfully";
+        public static string BrandsListed = "Brands listed successfully";
+
+        public static string ColorAdded = "Color added successfully";
+        public static string ColorDeleted = "Color deleted successfully";
+        public static string ColorUpdated = "Color updated successfully";
+        public static string ColorsListed = "Colors listed successfully";
+        public static string MaintenanceTime = "System is in maintenance";
+    }
+}
