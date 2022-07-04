24b374178e827f0fa6ba6e0ad7bfa6a7a1c8c04c
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-04 22:07:12 +0300

    fluent validation support added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,14 +24,8 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.Description.Length < 2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
-            if (car.DailyPrice <= 0)
-            {
-                return new ErrorResult(Messages.CarDailyPriceInvalid);
-            }
+            
+            ValidationTool.Validate(new CarValidator(), car);
 
             _carDal.Add(car);
 
