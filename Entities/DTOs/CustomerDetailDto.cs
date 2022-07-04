4ba153b6bca229923a947272445dec77dab3de75
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-04 16:06:10 +0300

    new dtos added

---
+++ Entities/DTOs/CustomerDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class CustomerDetailDto:IDto
+    {
+        public int CustomerId { get; set; }
+        public int UserId { get; set; } 
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
