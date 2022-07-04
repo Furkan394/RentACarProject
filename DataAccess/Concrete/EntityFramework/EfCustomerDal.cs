122b928ba93e820abc65a52ff66918a93339d750
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-04 16:07:59 +0300

    configured data access for new entities

---
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,34 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCustomerDal : EfEntityRepositoryBase<Customer, RentACarContext>, ICustomerDal
+    {
+        public List<CustomerDetailDto> GetCustomerDetails(Expression<Func<Customer, bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from cs in filter == null ? context.Customers : context.Customers.Where(filter)
+                             join u in context.Users
+                             on cs.UserId equals u.Id
+                             select new CustomerDetailDto
+                             {
+                                 CustomerId = cs.Id,
+                                 UserId = u.Id,
+                                 CustomerName = u.FirstName + " " + u.LastName,
+                                 CompanyName = cs.CompanyName
+                             };
+
+                return result.ToList();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,38 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var result = from rn in filter == null ? context.Rentals : context.Rentals.Where(filter)
+                             join cr in context.Cars on rn.CarId equals cr.Id
+                             join cs in context.Customers on rn.CustomerId equals cs.Id
+                             join br in context.Brands on cr.BrandId equals br.Id
+                             join u in context.Users on cs.UserId equals u.Id
+                             select new RentalDetailDto
+                             {
+                                 RentalId = rn.Id,
+                                 BrandName = br.BrandName,
+                                 CustomerName = u.FirstName + " " + u.LastName,
+                                 CompanyName = cs.CompanyName,
+                                 RentDate = rn.RentDate,
+                                 ReturnDate = rn.ReturnDate,
+                             };
+                return result.ToList();
+                   
+            }
+        }
+    }
+}
