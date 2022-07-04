70a6536e103df548e98df78b79f61bc29b67cd76
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-04 18:00:39 +0300

    cars controller added

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()   
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
