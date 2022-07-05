90487135a55a72209822eebca8cfaddd90284345
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-05 19:24:53 +0300

    car image business added

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,109 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Business;
+using Core.Utilities.Helpers;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+        IFileHelper _fileHelper;
+
+        public CarImageManager(ICarImageDal carImageDal, IFileHelper fileHelper)
+        {
+            _carImageDal = carImageDal;
+            _fileHelper = fileHelper;
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Add(IFormFile file, CarImage carImage)
+        {
+            var result = BusinessRules.Run(CheckIfCarImageLimitExceeded(carImage.CarId));
+
+            if (result != null)
+            {
+                return result;
+            }
+            carImage.ImagePath = _fileHelper.Upload(file, PathConstant.ImagesPath).Message;
+            carImage.ImageUploadDate = DateTime.Now;
+            _carImageDal.Add(carImage);
+            return new SuccessResult(Messages.CarImageAdded);
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Delete(CarImage carImage)
+        {
+            _fileHelper.Delete(PathConstant.ImagesPath + carImage.ImagePath);
+            _carImageDal.Delete(carImage);
+            return new SuccessResult(Messages.CarImageDeleted);
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(), Messages.CarImagesListed);
+        }
+
+        public IDataResult<CarImage> GetById(int imageId)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(i => i.Id == imageId));
+        }
+
+        public IDataResult<List<CarImage>> GetImagesByCarId(int carId)
+        {
+            var result = BusinessRules.Run(CheckIfCarImageExists(carId));
+            if (result != null)
+            {
+                return new ErrorDataResult<List<CarImage>>(GetDefaultImage(carId).Data);
+            }
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(i => i.CarId == carId));
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = _fileHelper.Update(file, PathConstant.ImagesPath + carImage.ImagePath, PathConstant.ImagesPath).Message;
+            _carImageDal.Update(carImage);
+            return new SuccessResult(Messages.CarImageUpdated);
+        }
+
+        private IResult CheckIfCarImageLimitExceeded(int carId)
+        {
+            var result = _carImageDal.GetAll(i => i.CarId == carId).Count;
+            if (result > 5)
+            {
+                return new ErrorResult(Messages.CarImageLimitExceeded);
+            }
+
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCarImageExists(int carId)
+        {
+            var result = _carImageDal.GetAll(i => i.CarId == carId).Count;
+            if (result < 0)
+            {
+                return new ErrorResult(Messages.CarImageDoesntExists);
+            }
+
+            return new SuccessResult();
+        }
+
+        private IDataResult<List<CarImage>> GetDefaultImage(int carId)
+        {
+            List<CarImage> carImage = new List<CarImage>();
+            carImage.Add(new CarImage { CarId = carId, ImageUploadDate = DateTime.Now, ImagePath = "default.jpg" });
+            return new SuccessDataResult<List<CarImage>>(carImage);
+        }
+    }
+}
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -3,6 +3,7 @@ using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
+using Core.Utilities.Helpers;
 using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
@@ -22,6 +23,7 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
             builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
             builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
+            builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
 
             builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
             builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
@@ -29,6 +31,9 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
             builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
             builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
+            builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
+
+            builder.RegisterType<FileHelperManager>().As<IFileHelper>().SingleInstance();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
