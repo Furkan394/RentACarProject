e13735c1e7de02beef3e441cf4b78bceda3c4b1e
Furkan394 <furkanncakiroglu@gmail.com>
2022-07-07 14:05:08 +0300

    user business refactored

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -10,6 +10,8 @@ namespace Business.Abstract
     {
         IDataResult<List<User>> GetAll();
         IDataResult<User> GetById(int userId);
+        List<OperationClaim> GetClaims(User user);
+        User GetByMail(string email);
         IResult Add(User user);
         IResult Update(User user);
         IResult Delete(User user);
